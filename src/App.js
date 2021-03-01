bf8f47df70e7da5a19438add0b13fd76842ae79e
Scott Choi <ascottychoi@gmail.com>
2021-03-01 00:33:21 -0800

    Added dropdowns for Enemy Buy and Round Outcome

--- src/App.js
+++ src/App.js
@@ -12,55 +12,98 @@ import {
   Button,
   Progress,
   Badge,
-  //ButtonGroup
+  ButtonGroup,
+  Icon,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Select
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
-import { MinusIcon } from '@chakra-ui/icons'
-
+import { FaMinus } from 'react-icons/fa';
 
 function App() {
   return (
-
+ 
     <ChakraProvider theme={theme}>
-      <Box 
-        height="16" 
+      <Box
+        textAlign="center"
+        fontFamily="sans-serif"
+        fontSize="3xl"
+        height="16"
         justifyContent="center" 
         alignItems="center" 
-        display="flex">
+        display="flex"
 
-      <Button 
-        colorScheme="red" 
-        fontSize="3xl">
-        T
-      </Button> 
-      
-      <Button 
-        colorScheme="blue" 
-        fontSize="3xl"  
       >
-        CT
-      </Button>
+      Choose Starting Side
       </Box>
+      
+      <ButtonGroup
+        justifyContent="center"
+        display="flex"
+      >
+        <Button
+          colorScheme="red"
+          fontSize="3xl"
+          padding="6"
+          marginRight="5"
+        >
+          T
+        </Button>
+        <Button
+          colorScheme="blue"
+          fontSize="3xl"
+          padding="6"
+        >
+          CT
+        </Button>
+      </ButtonGroup> 
+
       <Box mx="100">
-        <Progress size="lg" value={2} max={30} min={1}></Progress>
+        <Progress size="lg" 
+          value={2}
+          max={30} 
+          min={1}
+          marginTop="7"
+        > </Progress>
       </Box>
 
-      <Flex>
+      <Flex marginTop="5">
         <Box>
-          <MinusIcon />
-          <MinusIcon />
-          <MinusIcon />
-          <MinusIcon />
+          <Icon as={FaMinus} />
+          <Icon as={FaMinus} />
+          <Icon as={FaMinus} />
+          <Icon as={FaMinus} />
         </Box>
         <Spacer />
         <Box>
-          <MinusIcon />
-          <MinusIcon />
-          <MinusIcon />
-          <MinusIcon />
+          <Icon as={FaMinus} />
+          <Icon as={FaMinus} />
+          <Icon as={FaMinus} />
+          <Icon as={FaMinus} />
         </Box>
       </Flex>
+
+      <FormControl id="enemyBuy" marginTop="6" width="48">
+        <FormLabel>Enemy Buy</FormLabel>
+        <Select placeholder="Select Enemy Buy">
+          <option>Full Buy</option>
+          <option>Full Save</option>
+          <option>Half Buy</option>
+        </Select>
+      </FormControl>
+
+      <FormControl id="roundOutcome" marginTop="6" width="48">
+        <FormLabel>Round Outcome</FormLabel>
+        <Select placeholder="Round Outcome">
+          <option>Win</option>
+          <option>Lose</option>
+          <option>Enemy Team T-side Bomb Explodes</option>
+        </Select>
+      </FormControl>
 {/*
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
